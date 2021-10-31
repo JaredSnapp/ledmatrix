95bf5298054c04b70b06a1ee787c7d8b57587122
jaredsnapp <jaredasnapp@gmail.com>
2021-10-31 16:26:33 -0400

    updated pinouts

--- SimpleWifiServer.ino
+++ SimpleWifiServer.ino
@@ -19,14 +19,23 @@
 // Pin A4 works on the Adafruit Metro M4 (if using the Adafruit RGB
 // Matrix Shield, cut trace between CLK pads and run a wire to A4).
 
-#define CLK  8   // USE THIS ON ADAFRUIT METRO M0, etc.
+//#define CLK  8   // USE THIS ON ADAFRUIT METRO M0, etc.
 //#define CLK A4 // USE THIS ON METRO M4 (not M0)
 //#define CLK 11 // USE THIS ON ARDUINO MEGA
-#define OE   9
-#define LAT 10
-#define A   A0
-#define B   A1
-#define C   A2
+//#define OE   9
+//#define LAT 10
+//#define A   A0
+//#define B   A1
+//#define C   A2
+
+// Metro ESP32-S2
+#define CLK  13
+#define OE   14
+#define LAT 15
+#define A   17
+#define B   18
+#define C   1
+
 
 // Last parameter = 'true' enables double-buffering, for flicker-free,
 // buttery smooth animation.  Note that NOTHING WILL SHOW ON THE DISPLAY
