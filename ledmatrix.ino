5d9304abed13a77d708570a372a8ea568e33d2ee
jaredsnapp <jaredasnapp@gmail.com>
2021-11-01 01:23:17 -0400

    functional uploaded and tested. only red

---
+++ ledmatrix.ino
@@ -0,0 +1,275 @@
+// testshapes demo for RGBmatrixPanel library.
+// Demonstrates the drawing abilities of the RGBmatrixPanel library.
+// For 32x64 RGB LED matrix.
+
+// WILL NOT FIT on ARDUINO UNO -- requires a Mega, M0 or M4 board
+
+#include "RGBmatrixPanel.h"
+
+// Most of the signal pins are configurable, but the CLK pin has some
+// special constraints.  On 8-bit AVR boards it must be on PORTB...
+// Pin 11 works on the Arduino Mega.  On 32-bit SAMD boards it must be
+// on the same PORT as the RGB data pins (D2-D7)...
+// Pin 8 works on the Adafruit Metro M0 or Arduino Zero,
+// Pin A4 works on the Adafruit Metro M4 (if using the Adafruit RGB
+// Matrix Shield, cut trace between CLK pads and run a wire to A4).
+
+// Metro ESP32-S2
+#define CLK  13
+#define OE   14
+#define LAT 15
+#define A   17
+#define B   18
+#define C   1
+#define D   2
+
+RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false, 64);
+
+void setup() {
+
+  matrix.begin();
+
+  // draw a pixel in solid white
+  matrix.drawPixel(0, 0, matrix.Color333(7, 7, 7));
+  delay(500);
+
+  // fix the screen with green
+  matrix.fillRect(0, 0, matrix.width(), matrix.height(), matrix.Color333(0, 7, 0));
+  delay(500);
+
+  // draw a box in yellow
+  matrix.drawRect(0, 0, matrix.width(), matrix.height(), matrix.Color333(7, 7, 0));
+  delay(500);
+
+  // draw an 'X' in red
+  matrix.drawLine(0, 0, matrix.width()-1, matrix.height()-1, matrix.Color333(7, 0, 0));
+  matrix.drawLine(matrix.width()-1, 0, 0, matrix.height()-1, matrix.Color333(7, 0, 0));
+  delay(500);
+
+  // draw a blue circle
+  matrix.drawCircle(10, 10, 10, matrix.Color333(0, 0, 7));
+  delay(500);
+
+  // fill a violet circle
+  matrix.fillCircle(40, 21, 10, matrix.Color333(7, 0, 7));
+  delay(500);
+
+  // fill the screen with 'black'
+  matrix.fillScreen(matrix.Color333(0, 0, 0));
+
+  // draw some text!
+  matrix.setTextSize(1);     // size 1 == 8 pixels high
+  matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
+
+  matrix.setCursor(8, 0);    // start at top left, with 8 pixel of spacing
+  uint8_t w = 0;
+  char *str = "AdafruitIndustries";
+  for (w=0; w<8; w++) {
+    matrix.setTextColor(Wheel(w));
+    matrix.print(str[w]);
+  }
+  matrix.setCursor(2, 8);    // next line
+  for (w=8; w<18; w++) {
+    matrix.setTextColor(Wheel(w));
+    matrix.print(str[w]);
+  }
+  matrix.println();
+  //matrix.setTextColor(matrix.Color333(4,4,4));
+  //matrix.println("Industries");
+  matrix.setTextColor(matrix.Color333(7,7,7));
+  matrix.println("LED MATRIX!");
+
+  // print each letter with a rainbow color
+  matrix.setTextColor(matrix.Color333(7,0,0));
+  matrix.print('3');
+  matrix.setTextColor(matrix.Color333(7,4,0));
+  matrix.print('2');
+  matrix.setTextColor(matrix.Color333(7,7,0));
+  matrix.print('x');
+  matrix.setTextColor(matrix.Color333(4,7,0));
+  matrix.print('6');
+  matrix.setTextColor(matrix.Color333(0,7,0));
+  matrix.print('4');
+  matrix.setCursor(34, 24);
+  matrix.setTextColor(matrix.Color333(0,7,7));
+  matrix.print('*');
+  matrix.setTextColor(matrix.Color333(0,4,7));
+  matrix.print('R');
+  matrix.setTextColor(matrix.Color333(0,0,7));
+  matrix.print('G');
+  matrix.setTextColor(matrix.Color333(4,0,7));
+  matrix.print('B');
+  matrix.setTextColor(matrix.Color333(7,0,4));
+  matrix.print('*');
+
+  // whew!
+}
+
+void loop() {
+  // Do nothing -- image doesn't change
+}
+
+
+// Input a value 0 to 24 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint16_t Wheel(byte WheelPos) {
+  if(WheelPos < 8) {
+   return matrix.Color333(7 - WheelPos, WheelPos, 0);
+  } else if(WheelPos < 16) {
+   WheelPos -= 8;
+   return matrix.Color333(0, 7-WheelPos, WheelPos);
+  } else {
+   WheelPos -= 16;
+   return matrix.Color333(WheelPos, 0, 7 - WheelPos);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ WiFi Web Server LED Blink
+
+ A simple web server that lets you blink an LED via the web.
+ This sketch will print the IP address of your WiFi Shield (once connected)
+ to the Serial monitor. From there, you can open that address in a web browser
+ to turn on and off the LED on pin 5.
+
+ If the IP address of your shield is yourAddress:
+ http://yourAddress/H turns the LED on
+ http://yourAddress/L turns it off
+
+ This example is written for a network using WPA encryption. For
+ WEP or WPA, change the Wifi.begin() call accordingly.
+
+ Circuit:
+ * WiFi shield attached
+ * LED attached to pin 5
+
+ created for arduino 25 Nov 2012
+ by Tom Igoe
+
+ported for sparkfun esp32 
+31.01.2017 by Jan Hendrik Berlin
+ 
+ */
+
+/*
+
+#include <WiFi.h>
+#include "Adafruit_GFX.h"
+#include "RGB-matrix-Panel-master/RGBmatrixPanel.h"
+
+const char* ssid     = "yourssid";
+const char* password = "yourpasswd";
+
+WiFiServer server(80);
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(5, OUTPUT);      // set the LED pin mode
+
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    
+    server.begin();
+
+}
+
+int value = 0;
+
+void loop(){
+ WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 5 on.<br>");
+            client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 5 off.<br>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(5, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(5, LOW);                // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
+}
+
+
+
+*/
